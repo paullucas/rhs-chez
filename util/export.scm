60b1c224042ace56a7460ff27e7aab2279468b93
Rohan Drape <rd@slavepianos.org>
2008-03-13 03:29:18 -0700

    Write utility to generate plt module
    darcs-hash:24ad7aa6c1dffb3220476ade72d2ca15cdc42b43

--- util/export.scm
+++ util/export.scm
@@ -31,6 +31,28 @@
 	      (import (rnrs base))
 	      ,@xs)))
 
+(define in-plt
+  (list 'compose
+	'filter
+	'foldl
+	'foldr
+	'length
+	'null?
+	'reverse))
+
+(define hscheme-plt
+  (let ((xs (all-values "../hscheme.scm")))
+    `(module hscheme scheme/base
+	     (provide (all-defined-out))
+	     ,@(filter 
+		(lambda (x) 
+		  (not (elem (head (tail x)) in-plt))) 
+		xs))))
+
 (call-with-output-file "../r6rs/hscheme.scm"
   (lambda (p)
     (write hscheme-r6rs p)))
+
+(call-with-output-file "../plt/hscheme.ss"
+  (lambda (p)
+    (write hscheme-plt p)))
