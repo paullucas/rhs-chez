635144a725362b2d2a475ec9b540b1e2ea667fbe
Rohan Drape <rd@slavepianos.org>
2008-03-17 05:48:31 -0700

    Add min and max
    darcs-hash:b1f4fdf72be5a147845608e5d50de2eb954d4149

--- src/data/ord.scm
+++ src/data/ord.scm
@@ -9,3 +9,13 @@
     (cond ((> x y) gt)
           ((< x y) lt)
           (else eq))))
+
+;; max :: a -> a -> a
+(define max
+  (lambda (x y)
+    (if (> x y) x y)))
+
+;; min :: a -> a -> a
+(define min
+  (lambda (x y)
+    (if (< x y) x y)))
